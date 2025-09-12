f5953628f5563b04ab2b8b9ed0ba1ed235309b21
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-09-13 05:04:02 +0530

    Initialised project and loaded static files.

---
+++ firstDjango/firstDjango/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    #return HttpResponse("Hello, world. You're at the home page.")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("Hello, world. You're at the about page.")
+
+def contact(request):
+    return HttpResponse("Hello, world. You're at the contact page.")
